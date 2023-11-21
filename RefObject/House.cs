1ab2cb6501981f0a38a4e1926e2a42c5c30143b1
Tony Collins <tjc@eltham-college.org.uk>
2023-11-21 11:56:46 +0000

    Add project files.

---
+++ RefObject/House.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefObject
+{
+    internal class House
+    {
+        private string house;
+        public House(string h)
+        {
+            house = h;
+        }
+
+        public void PrintHouse()
+        {
+            Console.WriteLine(house);
+        }
+    }
+}
---
+++ RefObject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RefObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            House h = new House("Collins' House");
+            UpdateHouse(ref h);
+            h.PrintHouse();
+
+        }
+
+        // A reference to the reference that points to "Collins's house is changed
+        // The reference can therefore be changed to reference "Manuell's Housae"
+        // You rarely need to pass references to references
+        // the use of ref in old AQA skeleton code is a mistake
+
+        static void UpdateHouse(ref House h)
+        {
+            h = new House("Manuell's House");
+
+        }
+    }
+}
